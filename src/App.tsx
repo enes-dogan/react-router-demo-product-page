a559c32d26269e713b1a210601012f7a5071d0cb
enes-dogan <enesdogan210@gmail.com>
2024-01-09 16:53:18 +0300

    testing basic routes and rendering jsx accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/Home.tsx';
+import ProductsPage from './pages/Products.tsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/test', element: <h1>TESTING</h1> },
+  { path: '/products', element: <ProductsPage /> },
+]);
+
 export default function App() {
   return (
     <>
-      <div>
-        <h1>APP</h1>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <a href="/products">Go To Products</a>
+    </>
+  );
+}
